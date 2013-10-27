ce4058d282bd9be933b4f49874fa38cb443f1229
Angelo Naselli <anaselli@linux.it>
2013-10-27 11:01:34 +0100

    Fixed libyui-qt dependency

--- PROJECTINFO.cmake
+++ PROJECTINFO.cmake
@@ -7,8 +7,7 @@ SET( BASELIB		"yui" )		# don't change this
 SET( SUBDIRS		src )	        # set the subdirs where your code is located.
 SET( PLUGINNAME		"mga-qt" )		# define the your plugin's name, e.g. {gtk, ncurses-pkg, qt-graph}
 SET( LIB_DEPS		Qt )		# define the plugin's dependecies CMake should look for separated with spaces; libyui-internal deps go in the next line
-SET( INTERNAL_DEPS	Libyui )	# include libyui or it's plugins (if needed), too!
-#SET( INTERNAL_DEPS	Libyui libyui-mga-qt )	# include libyui or it's plugins (if needed), too!
+SET( INTERNAL_DEPS	Libyui Libyui-qt)	# include libyui or it's plugins (if needed), too!
 SET( LIB_LINKER		yui-mga yui-qt QtCore QtGui X11 fontconfig dl pthread )	# define the libs to link against with their -l name for separated with spaces, e.g. pthread dl; include libyui or it's plugins (if needed), too!
 #SET( EXTRA_INCLUDES	)		# set include-dir which are not picked by CMake automagically here.
 SET( PROGSUBDIR		"" )		# define your programm's name if the plugin is made for it, only. e.g. "YaST2" for {gtk,ncurses,qt}-pkg.
